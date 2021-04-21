c6a6e412ef46cbc69d680ad4dcd9e8a2764f657f
sansontwo <alissonsandes@hotmail.com>
2021-04-21 16:39:13 -0300

    5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -109,19 +109,9 @@
       async submit () {
         this.$v.$touch()
         if (this.isFormValidated()){
-          let headers= new Headers({
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Request-Method": "GET",
-                "Accept": "*"
-            })
-          let myInit = { method: 'GET',
-               headers: headers,
-               mode: 'cors',
-               cache: 'default' };
-
           let nome = this.name
           let cpf = this.cpf
-          let response = await fetch(`https://cobpag-api.vercel.app/api/buscarcliente?nome=${encodeURI(nome)}&cpf=${encodeURI(cpf)}`,myInit)
+          let response = await fetch(`https://cobpag-api.vercel.app/api/buscarcliente?nome=${encodeURI(nome)}&cpf=${encodeURI(cpf)}`)
           console.log(response)
         }
       },
